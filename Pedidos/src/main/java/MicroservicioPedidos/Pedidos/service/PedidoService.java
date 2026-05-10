0bccc6fc4af84f358cf579c71c65747f80b149f3
TheTrup <juanjorqueramendez@gmail.com>
2026-05-10 04:07:45 -0400

    Eliminacion de archivos basura

--- Pedidos/src/main/java/MicroservicioPedidos/Pedidos/service/PedidoService.java
+++ Pedidos/src/main/java/MicroservicioPedidos/Pedidos/service/PedidoService.java
@@ -23,20 +23,20 @@ public class PedidoService {
     public Pedido registrarPedido(Pedido pedido) {
         log.info("Iniciando validación para el cliente con ID: {}", pedido.getClienteId());
         
-        // 1. Armamos la URL exacta hacia nuestro microservicio de Clientes (Puerto 8081)
+        // Armamos la URL exacta hacia nuestro microservicio de Clientes (Puerto 8081)
         String urlMicroservicioClientes = "http://localhost:8081/api/v1/clientes/" + pedido.getClienteId();
 
         try {
-            // 2. Hacemos una llamada GET "invisible" para ver si el cliente existe
+            // Hacemos una llamada GET "invisible" para ver si el cliente existe
             ResponseEntity<String> respuesta = restTemplate.getForEntity(urlMicroservicioClientes, String.class);
             
-            // 3. Si no hay error, el cliente existe. Guardamos el pedido.
+            // Si no hay error, el cliente existe. Guardamos el pedido.
             log.info("Cliente validado correctamente. Guardando pedido...");
             pedido.setEstado("CREADO");
             return pedidoRepository.save(pedido);
 
         } catch (HttpClientErrorException.NotFound e) {
-            // 4. Si el puerto 8081 nos responde un 404 (No encontrado), bloqueamos el pedido.
+            // Si el puerto 8081 nos responde un 404 (No encontrado), bloqueamos el pedido.
             log.error("Error: El cliente con ID {} no existe en la base de datos.", pedido.getClienteId());
             throw new RuntimeException("No se puede crear el pedido: El cliente no existe.");
         }
