39da688cb1d711c79e6c79f6118693d5494c93c6
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-05-10 17:13:55 -0400

    Se agrega el Microservicio DetallePedido

---
+++ DtePedido/src/main/java/DetallePedido/DtePedido/DtePedidoApplication.java
@@ -0,0 +1,13 @@
+package DetallePedido.DtePedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtePedidoApplication.class, args);
+	}
+
+}
---
+++ DtePedido/src/test/java/DetallePedido/DtePedido/DtePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package DetallePedido.DtePedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
