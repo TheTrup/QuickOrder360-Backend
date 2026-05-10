9e2900abdd82313f1a980f0a3302f0ca86c44a4e
TheTrup <juanjorqueramendez@gmail.com>
2026-05-10 02:52:26 -0400

    Refactorizacion de Producto.java con caso de stock negativo

--- demo/src/main/java/QuickOrder360/example/demo/model/Producto.java
+++ demo/src/main/java/QuickOrder360/example/demo/model/Producto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Entity
 @Data
-
+@Table(name = "productos")
 public class Producto {
 
     @Id
@@ -16,7 +16,12 @@ public class Producto {
     @NotBlank(message = "El nombre del producto es obligatorio")
     private String nombre;
 
+    @NotNull(message = "El precio es obligatorio")
     @Positive(message = "El precio debe ser mayor a cero")
     private Double precio;
+
+    @NotNull(message = "El stock es obligatorio")
+    @Min(value = 0, message = "El stock no puede ser negativo")
+    private Integer stock;
 
 }
