48b873ff895503a3aadaab7f58ddef02c2ad4b5f
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-05-10 23:37:17 -0400

    microservicio Pagos

---
+++ Pago/src/test/java/MicroServicioPagos/Pago/PagoApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroServicioPagos.Pago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
