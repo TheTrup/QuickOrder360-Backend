8913438fd50822d3f944d04e7fcb2e1bb79848b0
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-05-11 02:07:12 -0400

    Creacion de microservicio notificaciones

--- Notificaciones/src/main/java/MicroServicioNotificaciones/Notificaciones/NotificacionesApplication.java
+++ Notificaciones/src/main/java/MicroServicioNotificaciones/Notificaciones/NotificacionesApplication.java
@@ -2,6 +2,8 @@ package MicroServicioNotificaciones.Notificaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NotificacionesApplication {
@@ -9,5 +11,10 @@ public class NotificacionesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificacionesApplication.class, args);
 	}
+
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
